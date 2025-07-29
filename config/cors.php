49b15c983b78f692ec1215b8eb9fcbb2476c4579
LeonMwangakala <ljmwangakala@yahoo.com>
2025-07-29 21:41:09 +0000

    Update CORS configuration and composer dependencies

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173',
+        'http://localhost:3000',
+        'https://desk.kadirafiki.com',
+        'https://admin.kadirafiki.com',
+        'https://kadirafiki.com'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +36,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
-]; 
+];
